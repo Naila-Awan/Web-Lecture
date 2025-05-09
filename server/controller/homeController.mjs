fa9bf5054b3d923c05c773c43cb128c7d3bf267e
Naila Awan <125586761+Naila-Awan@users.noreply.github.com>
2025-05-09 12:28:42 +0500

    Delete server directory

--- server/controller/homeController.mjs
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-
-const showHomePage = (req, res) => {
-    res.send('Welcome to the home page');
-};
-
-export default showHomePage;
--- server/routes/homeRoute.mjs
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import showHomePage from '../controller/homeController.mjs';
-
-const router = express.Router();
-
-router.get('/', showHomePage);
-
-export default router;
